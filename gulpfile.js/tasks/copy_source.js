c21eceed1f3a748eb1583ff43e59738957af4f20
Justin Bakse <justin@tsfim.com>
2016-08-22 14:27:26 -0400

    added publishing

---
+++ gulpfile.js/tasks/copy_source.js
@@ -0,0 +1,13 @@
+var browserSync = require('browser-sync');
+var changed     = require('gulp-changed');
+var config      = require('../config/copy_source');
+var gulp        = require('gulp');
+
+console.log(config);
+
+gulp.task('copy_source', function() {
+  return gulp.src(config.src)
+    .pipe(changed(config.dest)) // Ignore unchanged files
+    .pipe(gulp.dest(config.dest))
+    .pipe(browserSync.reload({stream:true}));
+});
